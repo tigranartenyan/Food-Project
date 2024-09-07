b4518b6def9ac5cb0ab7051e48c6ede44271917a
Tigran Artenyan <tigran2924@gmail.com>
2024-09-07 18:36:52 +0400

    Created tab logic, functions that show and hide content, and added new CSS classes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const tabHeaders = document.querySelectorAll(".tabheader__item");    
+    const tabHeadersParent = document.querySelector(".tabheader__items");    
+    const tabContents = document.querySelectorAll(".tabcontent");  
+    
+    function hideContent() {
+        tabContents.forEach(item => {
+            item.classList.add("hide");
+            item.classList.remove("show");
+        })
+        tabHeaders.forEach(item => {
+            item.classList.remove("tabheader__item_active");
+        })
+    }
+
+    function showContent(i = 0) {
+        tabContents[i].classList.add("show");
+        tabContents[i].classList.remove("hide");
+        tabHeaders[i].classList.add("tabheader__item_active");
+    }
+
+    hideContent();
+    showContent();
+
+    tabHeadersParent.addEventListener("click", (e) => {
+        if(e.target && e.target.matches(".tabheader__item")) {
+            tabHeaders.forEach((item, index) => {
+                if(e.target === item) {
+                    hideContent();
+                    showContent(index);
+                }
+            })
+        }
+    })
+});
