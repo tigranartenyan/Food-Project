a0c112c453229b95d626b4fe87e6c3075734d45d
Tigran Artenyan <tigran2924@gmail.com>
2024-09-14 18:41:47 +0400

    Added a timer

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
+    // tabs
     const tabHeaders = document.querySelectorAll(".tabheader__item");    
     const tabHeadersParent = document.querySelector(".tabheader__items");    
     const tabContents = document.querySelectorAll(".tabcontent");  
@@ -34,4 +35,54 @@ document.addEventListener("DOMContentLoaded", () => {
             })
         }
     })
+
+    // timer
+    const deadline = Date.parse("2024-09-15");
+    const setZero = n => n >= 0 && n < 10 ? `0${n}` : n;
+    
+    function getLeftTime(endTime) {
+        let todayDate = Date.now();
+        let diff = deadline - todayDate;
+        let days, hours, minutes, seconds;
+
+        if(diff <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } 
+        else {
+            days = Math.floor((diff / 1000 / 60 / 60 / 24));
+            hours = Math.floor((diff / 1000 / 60 / 60) % 24);
+            minutes = Math.floor((diff / 1000 / 60) % 60);
+            seconds = Math.floor((diff / 1000) % 60);
+        }
+
+        return {diff, days, hours, minutes, seconds}
+    }   
+
+    function setTimer(endTime) {
+        const daysElem = document.querySelector("#days");
+        const hoursElem = document.querySelector("#hours");
+        const minutesElem = document.querySelector("#minutes");
+        const secondsElem = document.querySelector("#seconds");
+
+        const timerID = setInterval(updateTimer, 1000);
+        
+        function updateTimer() {
+            const {diff, days, hours, minutes, seconds} = getLeftTime(endTime);
+
+            console.log(daysElem.textContent);
+            daysElem.textContent = setZero(days);
+            hoursElem.textContent = setZero(hours);
+            minutesElem.textContent = setZero(minutes);
+            secondsElem.textContent = setZero(seconds);
+
+            if(diff <= 0) {
+                clearInterval(timerID);
+            }
+        }        
+    }
+
+    setTimer(deadline);
 });
