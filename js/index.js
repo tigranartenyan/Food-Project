7a1c6e3002eb6d95e25a9447f7499317a66c96eb
Tigran Artenyan <tigran2924@gmail.com>
2024-10-13 23:17:30 +0400

    Created a menu tab with using ES6 Class

--- js/index.js
+++ js/index.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
         function updateTimer() {
             const {diff, days, hours, minutes, seconds} = getLeftTime(endTime);
 
-            console.log(daysElem.textContent);
+            // console.log(daysElem.textContent);
             daysElem.textContent = setZero(days);
             hoursElem.textContent = setZero(hours);
             minutesElem.textContent = setZero(minutes);
@@ -86,7 +86,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     setTimer(deadline);
 
-    // modal 
+    // modal
     const modalOpenTriggers = document.querySelectorAll("[data-modal-open]");
     const modalCloseTrigger = document.querySelector("[data-modal-close]");
     const modal = document.querySelector(".modal");
@@ -126,4 +126,58 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
     window.addEventListener("scroll", showModalByScroll);
+
+    // Menu 
+    const menuItems = document.querySelectorAll('.menu__items');
+    const parentItem = document.querySelector(".menu__field .container");
+    class Menu {
+        constructor(img, title, description, price) {
+            this.img = img;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+        }
+        showMenu() {
+            const element = document.createElement("div");
+            if(menuItems.length === 0) element.classList.add("menu__item");
+            element.innerHTML = `
+                <img src=${this.img} alt="poster"/>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">
+                    ${this.description}
+                </div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total">
+                        <span>${this.price}</span> грн/день
+                    </div>
+                </div>
+            `;
+           parentItem.append(element);
+           
+        }
+    }
+
+    let menu1 = new Menu(
+    "../img/tabs/vegy.jpg",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    550);
+
+    let menu2 = new Menu(
+    "../img/tabs/elite.jpg",
+    "Меню “Премиум”",
+    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+    210);
+
+    let menu3 = new Menu(
+    "../img/tabs/post.jpg",
+    'Меню "Постное"',
+    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+    430);
+
+    menu1.showMenu();
+    menu2.showMenu();
+    menu3.showMenu();
 });
